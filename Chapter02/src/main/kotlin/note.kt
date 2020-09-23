cf30ed10396dbd6fa5f39340edc17c0ea5b3e5a6
Mateus <mateus.vagner@gmail.com>
2020-09-23 09:07:37 -0300

    load file for project

---
+++ Chapter02/src/main/kotlin/note.kt
@@ -0,0 +1,30 @@
+import java.time.LocalDateTime
+
+
+// Store the next available id for all new notes
+//last_id = 0
+
+
+class Note {
+/*
+ *Represent a note in the notebook. Match against a string in searches and store tags for each note.
+ */
+
+//    var memo: String
+//    var tags: Strig = ""
+//    var date: Date  = LocalDateTime.now()
+
+
+//    fun match(filter: String): Boolean {
+    /*
+     * Determine if this note matches de filter text.
+     * Return true if it matches, false otherwise
+     *
+     * Search is case sensitive and matches both text and tags.
+     */
+
+//        return filter.contains(self.memo) || filter.contains(self.tags)
+
+//}
+}
+
