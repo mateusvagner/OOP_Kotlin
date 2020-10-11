7e6c35357f8f9f14dedcbfc85dae27635fa1af4c
Mateus <mateus.vagner@gmail.com>
2020-10-11 20:06:19 -0300

    Grader finished

--- Chapter03/src/main/kotlin/grader.kt
+++ Chapter03/src/main/kotlin/grader.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Grader {
 
     var studentGrades = mutableMapOf<String, AssignmentGrader>()
@@ -5,7 +7,7 @@ class Grader {
 
     fun register(assignment: (String) -> Assignment) {
 
-        val id = 1
+        val id = UUID.randomUUID().hashCode()
         assignmentClasses[id] = assignment
     }
 
@@ -15,6 +17,22 @@ class Grader {
     }
 
     fun getLesson(student: String) {
+        val assignment = studentGrades[student]
+        assignment?.lesson()
+    }
 
+    fun checkAssignment(student: String, code: String): Boolean? {
+        val assignment = studentGrades[student]
+        return assignment?.check(code)
+    }
+
+    fun assignmentSummary(student: String) {
+        val grader = studentGrades[student]
+        println(""" 
+            ${student}'s attempts att ${grader!!.assignment.javaClass.name}:
+             - Attempts: ${grader.attempts}
+             - Correct: ${grader.correctAttempts}
+             - Passed: ${grader.correctAttempts > 0}
+        """.trimIndent())
     }
 }
